f044984c2ba84664e5c471bfe6c76439c2eb632a
xfworld <xf.key@163.com>
2016-12-07 13:42:32 +0800

    更新配置文件

--- es6/main.js
+++ es6/main.js
@@ -1,8 +1,11 @@
 import Person from './Person';
 import BaseUrl from './BaseUrl';
+import 'amazeui/dist/js/amazeui.min';
+import 'amazeui/dist/css/amazeui.min.css';
+import './admin.css';
 // Template
-import titleTemplate from '../template/title.handlebars';
-
+//import titleTemplate from '../template/title.handlebars';
+import indexTemplate from '../template/index/index.handlebars';
 // let p = new Person('张三', 20);
 // document.write(p.say());
 // document.write(p.action());
@@ -10,15 +13,38 @@ import titleTemplate from '../template/title.handlebars';
 // let bUrl = new BaseUrl(currentRequest);
 // document.write("</br>"+bUrl.parseHost()+bUrl.parseApp());
 
+// document.addEventListener("DOMContentLoaded", function() {
+// 	var div = document.createElement('div');
+// 	div.innerHTML = titleTemplate({
+// 		username: "test",
+// 		books: [
+// 			{ title: "A book", synopsis: "With a description" },
+// 			{ title: "Another book", synopsis: "From a very good author" },
+// 			{ title: "Book without synopsis" }
+// 		]
+// 	});
+// 	document.body.appendChild(div);
+// });
+//
+//
 document.addEventListener("DOMContentLoaded", function() {
-	var div = document.createElement('div');
-	div.innerHTML = titleTemplate({
-		username: "test",
-		books: [
-			{ title: "A book", synopsis: "With a description" },
-			{ title: "Another book", synopsis: "From a very good author" },
-			{ title: "Book without synopsis" }
+	$('body').append(indexTemplate({
+		pages: [
+			{ type: "success", icon: "file-text",name:"新增页面",number:"2300" },
+			{ type: "warning", icon: "briefcase",name:"成交订单",number:"308" },
+			{ type: "danger", icon: "recycle",name:"昨日访问",number:"80082" },
+			{ type: "secondary", icon: "user-md",name:"在线用户",number:"3000" }
 		]
-	});
-	document.body.appendChild(div);
+	}));
+	// var div = document.createElement('div');
+	// div.innerHTML = indexTemplate({
+	// 	pages: [
+	// 		{ type: "success", icon: "file-text",name:"新增页面",number:"2300" },
+	// 		{ type: "warning", icon: "briefcase",name:"成交订单",number:"308" },
+	// 		{ type: "danger", icon: "recycle",name:"昨日访问",number:"80082" },
+	// 		{ type: "secondary", icon: "user-md",name:"在线用户",number:"3000" }
+	// 	]
+	// });
+	// document.body.appendChild(div);
+	$('#topbar-collapse .am-dropdown').dropdown();
 });
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -9,20 +9,28 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 // // Copy Webpack plugin
 // import CopyWebpackPlugin from 'copy-webpack-plugin';
+//  css
+//import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-    entry: "./es6/main.js", //入口文件
+    entry: "./es6/main.js", //入口文件,
     output: { //打包输出的文件
         // path: __dirname,
         // filename: "bundle.js"
         path: path.join(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
         //  publicPath: 'dist/', //模板、样式、脚本、图片等资源对应的server上的路径
-        filename: '[name].min.js', //每个页面对应的主js的生成配置
-        chunkFilename: '[id].chunk.min.js' // chunk生成的配置
+        filename: '[name].js', //每个页面对应的主js的生成配置
+        chunkFilename: '[id].chunk.js', // chunk生成的配置
+        sourceMapFilename:'[file].map'
+    },
+    //外部依赖 不参与编译
+    externals:{
+
     },
     module: {
         loaders: [{
             test: /\.handlebars$/,
+            exclude: /node_modules/,
             loader: 'handlebars-loader',
         }, {
             //test: path.join(__dirname, 'es6'),
@@ -31,8 +39,14 @@ export default {
             loader: 'babel-loader',
             query: {
                 presets: ['es2015'],
-              //  plugins: ['transform-runtime']
+                //  plugins: ['transform-runtime']
             }
+        }, { // 配置css加载，用sass的话，就配置sass的加载器就好
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, { // 配置字体文件加载
+            test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+            loader: 'file-loader'
         }]
     },
     resolve: { // 现在你require文件的时候可以直接使用require('file')，不用使用require('file.coffee')
@@ -41,11 +55,13 @@ export default {
     node: {
         fs: 'empty'
     },
+    debug: true,
+    devtool: 'source-map',
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: './index.html',
-            chunks: ['main'],
+            chunks: ['main', 'vendor'],
             hash: true,
             minify: { //压缩HTML文件
                 removeComments: true, //移除HTML中的注释
@@ -56,15 +72,22 @@ export default {
             $: "jquery",
             jQuery: "jquery"
         }),
-        new webpack.HotModuleReplacementPlugin(), //开启热替换插件
-        new webpack.NoErrorsPlugin(),
-    //    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'), //这是妮第三方库打包生成的文件
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-            mangle: false,
-            compress: {
-                warnings: false
-            }
-        })
+           new webpack.HotModuleReplacementPlugin(), //开启热替换插件
+           new webpack.NoErrorsPlugin(),
+           new webpack.optimize.OccurenceOrderPlugin(),
+      //     new ExtractTextPlugin("[name].chunk.css"),   //单独使用style标签加载css并设置其路径
+        new webpack.optimize.CommonsChunkPlugin('vendor.bundle.js',3), //这是妮第三方库打包生成的文件
+        // new webpack.optimize.UglifyJsPlugin({
+        //     sourceMap: false,
+        //     mangle: false,
+        //     compress: {
+        //         warnings: false
+        //     }
+        // })
+        //,
+        // new CopyWebpackPlugin([{
+        //     from: __dirname + '/assets',
+        //     to: __dirname + '/dist/assets'
+        // }])
     ]
 };
