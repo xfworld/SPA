448e8ce1cd702e41d4763d24081dcf9c1214b428
xfworld <xf.key@163.com>
2016-12-07 15:23:02 +0800

    add CleanWebpackPlugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,10 +6,12 @@ import path from 'path';
 
 // HTML Webpack plugin
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+// clean Webpack plugin
+import CleanWebpackPlugin from 'clean-webpack-plugin';
 
 // // Copy Webpack plugin
 // import CopyWebpackPlugin from 'copy-webpack-plugin';
-//  css
+//  css loader: ExtractTextPlugin.extract('style', 'css!stylus')
 //import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
@@ -20,9 +22,9 @@ export default {
         path: path.join(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
         //  publicPath: 'dist/', //模板、样式、脚本、图片等资源对应的server上的路径
         //  filename: '[name].js', //每个页面对应的主js的生成配置
-        //      chunkFilename: '[id].chunk.js', // chunk生成的配置
-        filename: '[name].[chunkhash:8].bundle.js',
-        chunkFilename: '[id].[chunkhash:8].bundle.js',
+        //      chunkFilename: '[id].chunk.js', // chunk生成的配置 chunkhash
+        filename: '[name].[hash:8].bundle.js',
+        chunkFilename: '[id].[hash:8].bundle.js',
         //      sourceMapFilename:'[file].map'
     },
     //外部依赖 不参与编译
@@ -49,7 +51,12 @@ export default {
         }, { // 配置字体文件加载
             test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
             loader: 'file-loader'
-        }]
+        },
+        // {
+        //     test: /\.(png|jpg|jpeg|gif|woff|woff2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //     loader: 'url-loader?limit=1000'
+        // },
+      ]
     },
     resolve: { // 现在你require文件的时候可以直接使用require('file')，不用使用require('file.coffee')
         extensions: ['', '.js', '.json', '.css']
@@ -63,7 +70,7 @@ export default {
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: './index.html',
-            chunks: ['main', 'vendor'],
+            chunks: ['main'], //, 'vendor']
             hash: false,
             minify: { //压缩HTML文件
                 removeComments: true, //移除HTML中的注释
@@ -77,18 +84,26 @@ export default {
         new webpack.HotModuleReplacementPlugin(), //开启热替换插件
         new webpack.NoErrorsPlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
         //     new ExtractTextPlugin("[name].chunk.css"),   //单独使用style标签加载css并设置其路径
-        new webpack.optimize.CommonsChunkPlugin('vendor','vendor.[hash:8].bundle.js'), //这是妮第三方库打包生成的文件
+      //  new webpack.optimize.CommonsChunkPlugin('vendor','vendor.[hash:8].bundle.js'), //这是妮第三方库打包生成的文件
         new webpack.optimize.UglifyJsPlugin({
             sourceMap: false,
             mangle: false,
             compress: {
                 warnings: false
+            },
+            output:{
+                comments:false
             }
-        })
+        }),
         // new CopyWebpackPlugin([{
         //     from: __dirname + '/assets',
         //     to: __dirname + '/dist/assets'
         // }])
+        new CleanWebpackPlugin(['dist','build'], {
+            verbose: true,
+            dry: false
+        })
     ]
 };
